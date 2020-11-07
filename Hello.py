e88aa6bd02e2c8b228f2a0240ca7594e9be31bbf
Adolphin-Kitler <73878650+Adolphin-Kitler@users.noreply.github.com>
2020-11-07 12:20:15 +0700

    5

--- Hello.py
+++ Hello.py
@@ -8,6 +8,17 @@ pygame.display.set_caption("Experiment", "hello")
 walkRight = [pygame.image.load("R1.png"), pygame.image.load("R2.png"), pygame.image.load("R3.png"),
              pygame.image.load("R4.png"), pygame.image.load("R5.png"), pygame.image.load("R6.png"),
              pygame.image.load("R7.png"), pygame.image.load("R8.png"), pygame.image.load("R9.png")]
+
+walkLeft = [pygame.image.load("L1.png"), pygame.image.load("L2.png"), pygame.image.load("L3.png"),
+            pygame.image.load("L4.png"), pygame.image.load("L5.png"), pygame.image.load("L6.png"),
+            pygame.image.load("L7.png"), pygame.image.load("L8.png"), pygame.image.load("L9.png")]
+
+bg = pygame.image.load("bg2.jpg")
+
+char = pygame.image.load("standing.png")
+
+clock = pygame.time.Clock()
+
 #ctrl+K
 #ctrl+alt+k
 #ctrl+shift+k
