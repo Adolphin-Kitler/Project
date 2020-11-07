30c989bbf5c9e11ba71b1bddecf5336fa531026c
Adolphin-Kitler <73878650+Adolphin-Kitler@users.noreply.github.com>
2020-11-07 12:15:54 +0700

    4

--- Hello.py
+++ Hello.py
@@ -1,7 +1,13 @@
 import pygame
 
 pygame.init()
+
 win = pygame.display.set_mode((720, 480))
+pygame.display.set_caption("Experiment", "hello")
+
+walkRight = [pygame.image.load("R1.png"), pygame.image.load("R2.png"), pygame.image.load("R3.png"),
+             pygame.image.load("R4.png"), pygame.image.load("R5.png"), pygame.image.load("R6.png"),
+             pygame.image.load("R7.png"), pygame.image.load("R8.png"), pygame.image.load("R9.png")]
 #ctrl+K
 #ctrl+alt+k
 #ctrl+shift+k
